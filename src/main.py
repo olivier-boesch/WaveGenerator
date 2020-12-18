63da245454e8c16c377cf18fa1f21416b47bde22
olivier-boesch <olivier.boesch@ac-aix-marseille.fr>
2020-12-18 10:14:00 +0100

    added more smooth frequency change
    When continuous mode is on, frequency will change once only on generator.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
 
+Commande de cuve à vagues NOVA Physics
+
+Gui pour commande d'un arduino
 
 
 """
@@ -19,26 +22,32 @@ import wavegen_arduino
 
 
 class WaveGenApp(App):
+    """Main app class"""
     title = 'Waveform Generator'
     generator = wavegen_arduino.WaveGenArduino(None,Logger.info)
     repeat_event = None
     generator_started = False
 
     def on_start(self):
+        """what to do just before start ?"""
         if not self.generator.connect():
             d = Factory.MessagePopup()
             d.open()
 
     def on_stop(self):
-        self.generator.stop()
+        """what to do just before stop ?"""
+        if self.generator_started:
+            self.generator.stop()
         self.generator.disconnect()
 
     @staticmethod
     def format_text(val, dec):
+        """how to format displyed text in textinput"""
         fstr = "{:." + str(dec) + "f}"
         return fstr.format(val)
 
     def reflect_val(self, who, textinput, slider, min_val, max_val, dec):
+        """Reflect and constrain value between a textinput and a slider"""
         if textinput.text == self.format_text(slider.value, dec):
             return
         if (who == 'input') and (textinput.text != ''):
@@ -55,37 +64,46 @@ class WaveGenApp(App):
             textinput.text = self.format_text(slider.value, dec)
 
     def set_mode(self, is_burst=False):
-        if not is_burst:
+        """set ui possibilities regarding current mode"""
+        if not is_burst:  # continuous mode
             self.root.ids['pulse'].disabled = True
             self.root.ids['repeat_pulse'].disabled = True
-        else:
+        else:  # Burst mode
             self.root.ids['pulse'].disabled = False
             self.root.ids['repeat_pulse'].disabled = False
 
     def update_freq(self):
+        """update freq live when running in continuous mode"""
         if self.generator_started:
-            freq = self.root.ids['slider_freq'].value
+            Logger.info("Freq: updating frequency live")
+            freq = "{:.3f}".format(self.root.ids['slider_freq'].value)
             mode = self.root.ids['spinner_mode'].text
             if mode == 'Continuous':
                 self.set_generator(mode, freq, 0)
 
     def set_generator(self, mode, freq, n):
+        """start generator with right mode and characteristics"""
         if self.generator_started:
             self.stop_generator()
         if mode == 'Burst':
+            Logger.info("Op: Start Generator (Burst of {:s} pulses at {:s} Hz".format(n, freq))
             self.generator.burst(int(n), float(freq))
             if self.root.ids['checkbox_repeat_pulses'].active:
                 every = int(self.root.ids['spinner_repeat_pulses'].text[:-1])
+                Logger.info("Op: Sheduling repeat bursts every {:d} s".format(every))
                 if self.repeat_event is not None:
                     self.repeat_event.cancel()
                     self.repeat_event = None
                 self.repeat_event = Clock.schedule_interval(lambda dt: self.generator.burst(int(n), float(freq)), every)
             self.generator_started = True
         elif mode == 'Continuous':
+            Logger.info("Op: Start Generator (continuous at {:s} Hz".format(freq))
             self.generator.continuous(float(freq))
             self.generator_started = True
 
     def stop_generator(self):
+        """stop generator"""
+        Logger.info("Op: Stop Generator")
         self.generator_started = False
         self.generator.stop()
         if self.repeat_event is not None:
@@ -93,5 +111,6 @@ class WaveGenApp(App):
             self.repeat_event = None
 
 
+# create and start app
 app = WaveGenApp()
 app.run()
--- src/wavegen_arduino.py
+++ src/wavegen_arduino.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
+Commande de cuve à vagues NOVA Physics
 
+Gui pour commande d'un arduino
 
-
+Fichier librarie pour commande l'arduino
 """
 from serial import Serial
 from serial.tools import list_ports
@@ -10,12 +12,15 @@ import os
 
 
 class WaveGenArduino:
-    pulse_freq_conversion_factor = 3200
+    """Class to handle arduino as a square signal generator"""
+    pulse_freq_conversion_factor = 3200  # conversion factor from rotationnal freq to excitation freq
     __serial_connection = None
 
     def __init__(self, port=None, debug_func=None):
+        """init"""
         self._debug_func = debug_func
         self._port = port
+        # if port is not set, pick the first one with an arduino attached to it
         if (port == None):
             ports_list = self.list_arduino_ports()
             if ports_list != []:
@@ -23,10 +28,12 @@ class WaveGenArduino:
                 self.__d("Serial: port set to {:s}".format(self._port))
 
     def __d(self, s):
+        """more display in a standalone debug (no gui)"""
         if self._debug_func is not None:
             self._debug_func(s)
 
     def list_arduino_ports(self):
+        """list serial ports where an arduino is connected"""
         if os.name == 'posix':
             prefix = '/dev/'
         else:
@@ -40,6 +47,7 @@ class WaveGenArduino:
         return arduino_list
 
     def connect(self):
+        """Connect to the Arduino"""
         self.__serial_connection = Serial(self._port, baudrate=115200)
         if self.__serial_connection.is_open:
             self.__d("Serial: Connected")
@@ -47,35 +55,43 @@ class WaveGenArduino:
         return False
 
     def disconnect(self):
+        """Disconnect from Arduino"""
         if self.__serial_connection is not None:
             self.__serial_connection.close()
             self.__serial_connection = None
             self.__d("Serial: disconnected")
 
     def _send_command(self, cmd):
+        """Send command over serial connection"""
         bytes_cmd = bytes(cmd,'utf-8')
         self.__serial_connection.write(bytes_cmd)
         self.__d("Serial: Sent command {:s}".format(repr(bytes_cmd)))
 
     def _freq_to_motor_freq(self, freq):
+        """Convert frequencies (and number of burst) in the real ones"""
         return int(freq * self.pulse_freq_conversion_factor)
 
     def continuous(self, freq):
+        """start generator in a continous mode"""
         motor_pulse_freq = self._freq_to_motor_freq(freq)
         self._send_command("C{:d}\n".format(motor_pulse_freq))
 
     def burst(self, n, freq):
+        """make the generator start only for n pulse at the right freq"""
         motor_pulse_freq = self._freq_to_motor_freq(freq)
         n = self._freq_to_motor_freq(n)
         self._send_command("B{:d},{:d}\n".format(n, motor_pulse_freq))
 
     def stop(self):
+        """stops the generator"""
         self._send_command("S\n")
 
     def query_state(self):
+        """ask arduino to tell its state: 0-> stop, 1-> continous, 2-> Burst"""
         self._send_command("?\n")
 
 
+# Test code to run standalone
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
